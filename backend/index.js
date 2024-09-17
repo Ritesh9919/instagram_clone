38e9a35d54eb11a67da83b4c66023660fc5243b8
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-17 14:47:19 +0530

    created run the first server

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const PORT = 8000;
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
